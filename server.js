be41963a0f114c6f82fba8057719688aa7dde169
Cheryl Daniels <cheryld433@gmail.com>
2020-05-20 14:41:11 -0400

    modified files

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const port = 8080;
-const mainDir = path.join(__dirname, "/public");
+const PORT = process.env.PORT || 1965;
+const publicDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(mainDir, "notes.html"));
+    res.sendFile(path.join(publicDir, "notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
@@ -24,7 +24,7 @@ app.get("/api/notes/:id", function(req, res) {
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(mainDir, "index.html"));
+    res.sendFile(path.join(publicDir, "index.html"));
 });
 
 app.post("/api/notes", function(req, res) {
@@ -57,6 +57,6 @@ app.delete("/api/notes/:id", function(req, res) {
     res.json(savedNotes);
 })
 
-app.listen(port, function() {
-    console.log(`Now listening to port ${port}. Enjoy your stay!`);
+app.listen(PORT, function() {
+    console.log("App listenin on PORT:" + PORT);
 })
