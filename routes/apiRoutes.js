dd1d8ba332f07a08e36ba8ec56c67d8d52d245ea
Cheryl Daniels <cheryld433@gmail.com>
2020-05-22 01:08:04 -0400

    created writeToDBase funtion

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,19 @@
 const fs = require("fs");
 const savedNote = require("../db/db.json");
 
+module.exports = function(app) {
+    
+    function writeToDBase(notes){
+        
+        notes = JSON.stringify(notes);
+        console.log (notes);
+        fs.writeFileSync("./db/db.json", notes, function(err){
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+
+
+
+}
