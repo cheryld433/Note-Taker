f4cf0bb7426c801faeb0e5a1f95f079f3ed82196
Cheryl Daniels <cheryld433@gmail.com>
2020-05-22 00:52:36 -0400

    modified file

--- server.js
+++ server.js
@@ -1,62 +1,18 @@
+
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 1965;
-const publicDir = path.join(__dirname, "/public");
+const PORT = process.env.PORT || 2000;
 
-app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(publicDir, "notes.html"));
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(savedNotes[Number(req.params.id)]);
-});
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(publicDir, "index.html"));
-});
-
-app.post("/api/notes", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let newNote = req.body;
-    let uniqueID = (savedNotes.length).toString();
-    newNote.id = uniqueID;
-    savedNotes.push(newNote);
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-    console.log("Note saved:", newNote);
-    res.json(savedNotes);
-})
-
-app.delete("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteID = req.params.id;
-    let newID = 0;
-    console.log("Deleted note");
-    savedNotes = savedNotes.filter(currentNote => {
-        return currentNote.id != noteID;
-    })
-    
-    for (currentNote of savedNotes) {
-        currentNote.id = newID.toString();
-        newID++;
-    }
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-    res.json(savedNotes);
-})
+app.use(express.static("public"));
 
 app.listen(PORT, function() {
-    console.log("App listenin on PORT:" + PORT);
-})
+    console.log("App listening on PORT: " + PORT);
+});
